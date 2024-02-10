94f112b5f85db5f942e99e4d98d5cd327c3adb35
jlozadalegra <jlozadalegra@gmail.com>
2024-02-10 15:05:15 -0500

    init python

---
+++ trabajadores/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class trabajador(models.Model):
+    nombre = models.CharField(max_length=60)
+    papellido = models.CharField(max_length=30)
+    sapellido = models.CharField(max_length=30)
+    edad = models.CharField(max_length=2)
+    sexo = models.CharField(max_length=1)
+    ci = models.CharField(max_length=11)
+    note = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)    
